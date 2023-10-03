068b2e99bcec41cddc0830748d05174e7217704b
Nick Diplos <nickd321@gmail.com>
2023-10-03 14:29:01 +0100

    finished taks 8

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,5 +1,5 @@
 
-const { fetchArticleById, fetchArticles, fetchCommentsByArticleId } = require('../models/models.articles')
+const { fetchArticleById, fetchArticles, fetchCommentsByArticleId, updateArticleById } = require('../models/articles.models')
 
 exports.getArticleById = (req, res, next) => {
     fetchArticleById(req.params.article_id).then( ( result ) => {
@@ -18,4 +18,13 @@ exports.getCommentsByArticleId = (req, res, next) => {
     fetchCommentsByArticleId(req.params.article_id).then( (result) => {
         return res.status(200).send(result)
     }).catch( (err) => next(err) )
+}
+
+exports.patchArticleById = (req, res, next) => {
+    if (!req.body.inc_votes) next({status: 400, msg: "inc_votes key not found"})
+    if (Object.keys(req.body).length != 1) next({status: 400, msg: "Invalid object (must only have one key)"})
+    updateArticleById(req.params.article_id, req.body.inc_votes).then( (result) => {
+        res.status(200).send(result)
+    })
+    .catch( (err) => next(err) )
 }
--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,16 +1,15 @@
 
 
 exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status === 404) res.status(404).send({ msg: err.msg })
+    if (err.status === 404 || err.status === 400) res.status(err.status).send({ msg: err.msg })
     next(err)
 }
 
 exports.handlePSQLErrors = (err, req, res, next) => {
-    if (err.code === "22P02") res.status(400).send({ msg: "Invalid article_id" })
+    if (err.code === "22P02") res.status(400).send({ msg: "Invalid database input" })
     next(err)
 }
 
 exports.handle500Errors = (err, req, res, next) => {
     if (err) res.status(500).send({ msg: "Unhandled Error" })
-    next()
 }
